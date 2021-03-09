d5b50666c606b162382c762526859f3402be70be
arssi112 <43404511+arssi112@users.noreply.github.com>
2021-03-09 14:08:00 +0100

    Updated MainMenu
    Added a slider and a return button inside the OptionMenu

--- Dungeon-Run-Game/Assets/Scripts/MainMenu/MainMenu.cs
+++ Dungeon-Run-Game/Assets/Scripts/MainMenu/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
 
     public GameObject MainMenuPanel;
     public GameObject OptionPanel;
+
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,11 +16,19 @@ public class MainMenu : MonoBehaviour
 
     public void Option()
     {
-        if (OptionPanel != null)
+        if (OptionPanel != false)
         {
             OptionPanel.SetActive(true);
             MainMenuPanel.SetActive(false);
+        }
+    }
 
+    public void Return()
+    {
+        if (MainMenuPanel != false)
+        {
+            OptionPanel.SetActive(false);
+            MainMenuPanel.SetActive(true);
         }
     }
 
