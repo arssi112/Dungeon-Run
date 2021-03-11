ea7d4bd648a2ce0c4010d0ddd71a46860a785c2e
arssi112 <43404511+arssi112@users.noreply.github.com>
2021-03-11 08:40:11 +0100

    updated collision
    Player will now collide with walls and other objects

---
+++ Dungeon-Run-Game/Assets/Scripts/Game/Player/Player.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Vector2 velocity;
+    public LayerMask wallMask;
+
+    private bool walk, walk_left, walk_right, jump;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPlayerInput();
+        UpdatePlayerPosition();
+    }
+
+    void UpdatePlayerPosition()
+    {
+        Vector3 pos = transform.localPosition;
+        Vector3 scale = transform.localScale;
+
+        if (walk)
+        {
+            if (walk_left)
+            {
+                pos.x -= velocity.x * Time.deltaTime;
+                scale.x = -1;
+
+            }
+
+            if (walk_right)
+            {
+                pos.x += velocity.x * Time.deltaTime;
+                scale.x = 1;
+            }
+
+            pos = ChechkWallRays(pos, scale.x);
+        }
+
+        transform.localPosition = pos;
+        transform.localScale = scale;
+    }
+
+    void CheckPlayerInput()
+    {
+        bool input_left = Input.GetKey(KeyCode.A);
+        bool input_right = Input.GetKey(KeyCode.D);
+        bool input_space = Input.GetKeyDown(KeyCode.Space);
+
+        walk = input_left || input_right;
+
+        walk_left = input_left && !input_right;
+
+        walk_right = !input_left && input_right;
+
+        jump = input_space;
+
+    }
+
+    //collision checker
+    Vector3 ChechkWallRays(Vector3 pos, float direction)
+    {
+        //gives ray position pos.x + direction equals where the player pos is.
+        //pos.y takes the center pos and adds 1 pos from the top to avoid invalid collisions.
+        Vector2 originTop = new Vector2(pos.x + direction * .4f, pos.y + 1f - 0.2f);
+        Vector2 originMiddle = new Vector2(pos.x + direction * .4f, pos.y);
+        Vector2 originBottom = new Vector2(pos.x + direction * .4f, pos.y -1f + 0.2f);
+
+        RaycastHit2D wallTop = Physics2D.Raycast(originTop, new Vector2(direction, 0), velocity.x * Time.deltaTime, wallMask);
+        RaycastHit2D wallMiddle = Physics2D.Raycast(originMiddle, new Vector2(direction, 0), velocity.x * Time.deltaTime, wallMask);
+        RaycastHit2D wallBottom = Physics2D.Raycast(originBottom, new Vector2(direction, 0), velocity.x * Time.deltaTime, wallMask);
+
+        if (wallTop.collider != null || wallMiddle.collider != null || wallBottom.collider != null)
+        {
+            pos.x -= velocity.x * Time.deltaTime * direction;
+        }
+
+        return pos;
+    }
+}
