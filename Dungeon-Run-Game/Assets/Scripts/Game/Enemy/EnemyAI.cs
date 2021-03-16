0e7c5e928157b570be4d7293c9043c6ce1743ee5
arssi112 <43404511+arssi112@users.noreply.github.com>
2021-03-16 15:30:58 +0100

    EnemyAI(NOT DONE)
    started working on my enemy AI and the controlling between two points

---
+++ Dungeon-Run-Game/Assets/Scripts/Game/Enemy/EnemyAI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    //Reference to waypoints
+    public List<Transform> points;
+    //int value for the next point index
+    public int nextID;
+    //value of that applies to ID for changing
+    int IdChangeValue = 1;
+
+
+    private void Reset()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        //Make box collider trigger
+        GetComponent<BoxCollider2D>().isTrigger = true;
+        //Create root object
+        GameObject root = new GameObject(name + "_root");
+        //Reset pos of root to enemy object
+        root.transform.position = transform.position;
+        //Set enemy object as child of root
+        transform.SetParent(root.transform);
+        //Create waypoints object
+        GameObject waypoints = new GameObject("Waypoints");
+        //Reset waypoints pos to root
+
+        //make waypoints object child of root
+        waypoints.transform.SetParent(root.transform);
+        waypoints.transform.position = root.transform.position;
+        //create two points(gameobject) and reset their pos to waypoints objects
+        //Make the points children of waypoint object
+        GameObject p1 = new GameObject("Point1"); p1.transform.SetParent(waypoints.transform); p1.transform.position = root.transform.position;
+        GameObject p2 = new GameObject("Point2"); p2.transform.SetParent(waypoints.transform); p2.transform.position = root.transform.position;
+
+        //Init points list then add the points to it
+        points = new List<Transform>();
+        points.Add(p1.transform);
+        points.Add(p2.transform);
+    }
+}
